6084becc1f7f2079fc525589d9c598fac36e2392
Radek Šuráni <radek.surani@gmail.com>
2020-03-17 14:03:13 +0100

    Added attempt method

--- src/ConnectionInterface.php
+++ src/ConnectionInterface.php
@@ -8,10 +8,19 @@ use Surda\Ldap\Exception\InvalidCredentialsException;
 interface ConnectionInterface
 {
     /**
+     * @param string $username
+     * @param string $password
+     * @param bool   $bindAsUser
+     * @return bool
+     * @throws BindException
+     * @throws InvalidCredentialsException
+     */
+    public function attempt(string $username, string $password, bool $bindAsUser = FALSE): bool;
+
+    /**
      * @param string|null $dn
      * @param string|null $password
      * @throws BindException
-     * @throws InvalidCredentialsException
      */
     public function bind(?string $dn = NULL, ?string $password = NULL): void;
 
